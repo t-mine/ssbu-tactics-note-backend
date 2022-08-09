76cf6ae5be0fe6ae9dc4279f1045da99e91c9f94
t-mine <tomonori.mine.biz@gmail.com>
2022-08-09 20:56:41 +0900

    動画データ取得APIの実装(途中)

---
+++ src/api/getVideo.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const DynamoDb = require('../client/dynamoDBClient');
+const dynamoDb = DynamoDb.create();
+
+// 動画取得API
+module.exports.handler = async (event, context) => {
+  // 検索条件を作成
+  const params = {
+    TableName: 'video',
+  };
+
+  // 検索
+  let result;
+  try {
+    result = await dynamoDb.query(params).promise();
+    console.log('検索結果 : ' + JSON.stringify(result));
+  } catch (e) {
+    console.log('DynamoDB検索失敗');
+    console.log(e);
+    // レスポンスを返す
+    context.succeed({
+      statusCode: 500,
+      headers: { 'Access-Control-Allow-Origin': '*' },
+    });
+  }
+
+  // レスポンスを返す
+  context.succeed({
+    statusCode: 200,
+    headers: { 'Access-Control-Allow-Origin': '*' },
+    body: JSON.stringify(result.Items),
+  });
+};
